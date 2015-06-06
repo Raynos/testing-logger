be706fc097e91710473656f322c000a077207494
Raynos <raynos2@gmail.com>
2015-06-06 16:21:38 -0700

    index: remove var proto pattern

--- index.js
+++ index.js
@@ -46,47 +46,45 @@ function DebugLogtron(namespace, opts) {
     this._stream = this._backend.createStream();
 }
 
-var proto = DebugLogtron.prototype;
-
-proto._log = function _log(level, msg, meta, cb) {
+DebugLogtron.prototype._log = function _log(level, msg, meta, cb) {
     var logMessage = new LogMessage(level, msg, meta);
     LogMessage.isValid(logMessage);
 
     this._stream.write(logMessage, cb);
 };
 
-proto.whitelist = function whitelist(level, msg) {
-    this._backend.whitelist(level, msg);
-};
-
-proto.items = function items() {
-    return this._backend.records;
-};
-
-proto.trace = function trace(msg, meta, cb) {
+DebugLogtron.prototype.trace = function trace(msg, meta, cb) {
     this._log(LEVELS.trace, msg, meta, cb);
 };
 
-proto.debug = function debug(msg, meta, cb) {
+DebugLogtron.prototype.debug = function debug(msg, meta, cb) {
     this._log(LEVELS.debug, msg, meta, cb);
 };
 
-proto.info = function info(msg, meta, cb) {
+DebugLogtron.prototype.info = function info(msg, meta, cb) {
     this._log(LEVELS.info, msg, meta, cb);
 };
 
-proto.access = function access(msg, meta, cb) {
+DebugLogtron.prototype.access = function access(msg, meta, cb) {
     this._log(LEVELS.access, msg, meta, cb);
 };
 
-proto.warn = function warn(msg, meta, cb) {
+DebugLogtron.prototype.warn = function warn(msg, meta, cb) {
     this._log(LEVELS.warn, msg, meta, cb);
 };
 
-proto.error = function error(msg, meta, cb) {
+DebugLogtron.prototype.error = function error(msg, meta, cb) {
     this._log(LEVELS.error, msg, meta, cb);
 };
 
-proto.fatal = function fatal(msg, meta, cb) {
+DebugLogtron.prototype.fatal = function fatal(msg, meta, cb) {
     this._log(LEVELS.fatal, msg, meta, cb);
+};
+
+DebugLogtron.prototype.whitelist = function whitelist(level, msg) {
+    this._backend.whitelist(level, msg);
+};
+
+DebugLogtron.prototype.items = function items() {
+    return this._backend.records;
 };
