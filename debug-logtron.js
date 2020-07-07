a7d1aba61fb076b4b8e82707058117a4316411ad
Raynos <raynos2@gmail.com>
2020-07-07 16:33:32 +0200

    simplify even further

--- debug-logtron.js
+++ debug-logtron.js
@@ -7,8 +7,26 @@ var os = require('os')
 var Buffer = require('buffer').Buffer
 
 var DebugLogBackend = require('./debug-log-backend.js')
-var LEVELS_BY_VALUE = require('./levels.js').LEVELS_BY_VALUE
-var LEVELS_BY_NAME = require('./levels.js').LEVELS_BY_NAME
+
+var LEVELS_BY_NAME = {
+  trace: 10,
+  debug: 20,
+  info: 30,
+  access: 35,
+  warn: 40,
+  error: 50,
+  fatal: 60
+}
+
+var LEVELS_BY_VALUE = {
+  10: 'trace',
+  20: 'debug',
+  30: 'info',
+  35: 'access',
+  40: 'warn',
+  50: 'error',
+  60: 'fatal'
+}
 
 DebugLogtron.LogMessage = LogMessage
 module.exports = DebugLogtron
